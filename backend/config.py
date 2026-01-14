8dc4a2eed5bc4a79b7753812ae33af73bebbf721
Doruk Ersoy <doruk.ersoy.24@ucl.ac.uk>
2026-01-14 15:22:46 +0000

    edit config

--- backend/config.py
+++ backend/config.py
@@ -1,14 +1,13 @@
+# Lyric generation
 LYRIC_MODEL_PATH = "ibm-granite/granite-3.3-2b-instruct"
-MUSIC_MODEL_PATH = "facebook/musicgen-small"
-
-# Song structure constants
+LYRIC_MAX_TOKENS = 100
+TEMPERATURE = 0.7
+TOP_P = 0.9
 STANZA_COUNT = 2
 LINE_COUNT = 4
 SEQUENCE = "AABB"
 SYLLABLE_COUNT = 8
 
-# Generation parameters
-LYRIC_MAX_TOKENS = 100
+# Music generation
+MUSIC_MODEL_PATH = "facebook/musicgen-small"
 MUSIC_MAX_TOKENS = 1503
-TEMPERATURE = 0.7
-TOP_P = 0.9
